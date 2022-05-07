b4af330229210be08aae9b1d5f624013f86229d4
Stefan <s.vladulescu@gmail.com>
2022-05-07 12:39:21 +0300

    Kind of fixed the AI not placing ships.

--- script.js
+++ script.js
@@ -34,6 +34,7 @@ function Gameboard(){
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0]
     ]
+    this.totalHits = 0;
 }
 function Player(username){
     this.username = username;
@@ -46,6 +47,7 @@ function Player(username){
     }
 }
 function createUIgameboard(who){
+    //p is the player
     let p;
     const boardUI = document.createElement('div')
     boardUI.classList.add('board')
@@ -70,23 +72,35 @@ function createUIgameboard(who){
     }
 }
 function startGame(){
+    //checks if the game is already started
     if(isStarted==false){
+        //created player object
     let player1 = new Player(usernameInput.value);
+    //adds it to the players array for later easy acces globally
     players.push(player1)
+    //sets the player name to the input
     player1name.innerHTML = usernameInput.value;
+    //created the enemy object
     let enemy = new Player('Enemy AI');
+    //adds enemy to players array
     players.push(enemy)
+    //created gamebaord for player and enemy
     createUIgameboard('player');
     let playerGameboard = new Gameboard('player');
     let enemyGameboard = new Gameboard('AI');
     createUIgameboard('AI')
+    //adds the gameboards to an array for easy later acces
     gameboards.push(playerGameboard)
     gameboards.push(enemyGameboard)
+    //starts the AI function that places AI's ships
     AI();
+    //sets the game as started
     isStarted = true;
     } else{
+        //if the game is already started, show an error
         hitStatus.innerHTML = "The game has already started!";
     }
+    //shows the numbers of ships left for the player
     shipC1.innerHTML = players[0].ships.ship1;
     shipC2.innerHTML = players[0].ships.ship2;
     shipC3.innerHTML = players[0].ships.ship3;
@@ -107,15 +121,16 @@ function handleHit(i,j,p){
     }else if(p=="player"){console.log('player')}
 
 
-
+    //it uses ship counter to know which ship is selected currently and which are left to be placed
     if(p==='player'){
     if(shipCounter === 0){
-        
+            //checks if the move is a valid one
             let move = checkValidMove(i,j,4,p);
             if(move==true){
-
+                //places the ship
                 placeShipVertically(i,j,p,4);
             }else {
+                //show an error if the move is not valid
                 logsWindow.innerHTML = "Not a valid move";
             }
         
@@ -144,29 +159,43 @@ function handleHit(i,j,p){
                         }else {
                             logsWindow.innerHTML = "Not a valid move";
                         }            }else if(shipCounter === 7){
+                            //this makes sure you can't click anymore on the Player's board after you placed your ship
                 startRound();
             } 
 }else if(p=='AI'){
+    //if the gameboard that is clicked is AI's board, it runs the code that handles hits
     if(gameboards[1].board[i][j]=='s'){
+        //sets cell's background to green if the hit is successful
         document.getElementById(`cell${i}x${j}x${p}`).style.cssText = "background-color: green";
+        //show a confirmation message
         hitStatus.innerHTML = "You hit!";
+        //marks the position with an "X" inside the gamebaoard array
         gameboards[1].board[i][j]= 'x';
+        //removes the event listener so you can't click the same cell multiple times
         document.getElementById(`cell${i}x${j}x${p}`).removeAttribute('onclick')
+        //increase the hit counter
         players[1].hits++;
+        //checks if the game is won
         checkWinStatus();
+        //makes AI to hit
         aiHit();
      
 
     } else {
+        //it turns the background red if you miss the shot
         document.getElementById(`cell${i}x${j}x${p}`).style.cssText = "background-color: red";
+        //shows a message
         hitStatus.innerHTML = "You missed!";
+        //set the position as "m" in the gameboard array
         gameboards[1].board[i][j]= 'm';
+        //removes the event listener to stop player from clicking the same cell again
         document.getElementById(`cell${i}x${j}x${p}`).removeAttribute('onclick');
         checkWinStatus();
         aiHit();
  
 }
     } else {
+        //if you haven't places all ships, you get an error
         hitStatus.innerHTML = `Please place all your ships`
     }
 
@@ -186,17 +215,18 @@ function handleHit(i,j,p){
 let aiShipCounter = 0;
 function AI(){
     let p='AI'
-    i = Math.floor(Math.random() * (9 - 2 + 1) + 2);    
-    j = Math.floor(Math.random() * (9 - 2 + 1) + 2);  
+    //gets 2 random numbers that are used for AI's position
+    i = Math.floor(Math.random() * (8 - 2 + 1) + 2);    
+    j = Math.floor(Math.random() * (8 - 2 + 1) + 2);  
         function getNumbers(){
-            i = Math.floor(Math.random() * (9 - 2 + 1) + 2);    
-            j = Math.floor(Math.random() * (9 - 2 + 1) + 2);
-            console.log(aiShipCounter)    
+            i = Math.floor(Math.random() * (8 - 2 + 1) + 2);    
+            j = Math.floor(Math.random() * (8 - 2 + 1) + 2);
         }
         
         if(aiShipCounter === 0){
             getNumbers();
-            if(gameboards[1].board[i][j] === 's' || gameboards[1].board[i-1][j] === 's' || gameboards[1].board[i-2][j] === 's' || gameboards[1].board[i-3][j] === 's'){
+            console.log(i,j)
+            if(gameboards[1].board[i][j] === 's' || gameboards[1].board[i-1][j] === 's' || gameboards[1].board[i-2][j] === 's' || gameboards[1].board[i-3][j] === 's' || i<4){
                 console.log("You can't place your ship there!");
                 getNumbers();
                 AI();
@@ -210,9 +240,11 @@ function AI(){
 
         }else if(aiShipCounter === 1||aiShipCounter===2){
             getNumbers();
+            console.log(i,j)
             if(gameboards[1].board[i][j] === 's' || gameboards[1].board[i-1][j] === 's' || gameboards[1].board[i-2][j] === 's'){
                 console.log("You can't place your ship there!");
                 getNumbers();
+
                 AI()
             }else {
       
@@ -224,22 +256,25 @@ function AI(){
 
         }else if(aiShipCounter === 3||aiShipCounter===4){
             getNumbers();
+            console.log(i,j)
             if(gameboards[1].board[i][j] === 's' || gameboards[1].board[i-1][j] === 's'){
                 console.log("You can't place your ship there!");
                 getNumbers();
+
                 AI()
             }else {
   
-        
             aiShipPlace(i,j,p,2);
                 
             }   
 
         }else if(aiShipCounter === 5||aiShipCounter===6){
             getNumbers();
+            console.log(i,j)
             if(gameboards[1].board[i][j] === 's'){
                 console.log("You can't place your ship there!");
                 getNumbers();
+
                 AI()
             }else {
         
@@ -250,13 +285,24 @@ function AI(){
         }
 
 }
+function checkIfAIWords(){
+    let ships = countShipsAI();
+    if(ships<6){
+        AI();
+    }
+}
 function aiShipPlace(i,j,p,l){
-   
+    console.log(aiShipCounter)
     for(let a=l;a>0;a--){
-        document.getElementById(`cell${i}x${j}x${p}`).style.cssText = "background-color: lightgray";
+        
+        //marks the positions as "S" in the gameboard array
         gameboards[1].board[i][j] = 's';
+
         i--;
         aiShipCounter++;
+        gameboards[1].totalHits++;
+        checkIfAIWords();
+
         AI();
     }
     
@@ -293,13 +339,27 @@ function aiHit(){
 function startRound(){
     console.log('The round has started!')
     logsWindow.innerHTML = "The round has started! Attack your target!"
+   
 }
-
+//counts how many possible successful hits are on the AI's gameboard
+function countShipsAI(){
+    let count = 0;
+    for(let i=0;i<9;i++){
+        for(let j=0;j<9;j++){
+            if(gameboards[1].board[i][j]=='s'){
+                count++
+            }
+        }
+    }
+    return count;
+}
 function checkWinStatus(){
-
+    //if AI hits counter is 0, then the AI loses
+    let count = countShipsAI();
+    //for the player, it is working as intended so the number of possible hits is always 16
     if(players[0].hits==16){
         finishGame(players[1].username);
-    }else if(players[1].hits==16){
+    }else if(count==0){
         finishGame(players[0].username)
     }
     
@@ -316,6 +376,7 @@ function finishGame(name){
 }
 
 function rotateShip(){
+    //a simple function that changes the variable to the other direction on button press
     console.log("Working")
     if(direction == 'v'){
         direction = 'h';
@@ -328,6 +389,11 @@ function rotateShip(){
 
 
 function placeShipVertically(i,j,p,l){
+    let aiships = countShipsAI();
+    if(aiships==0){
+        aiShipCounter = 0;
+        AI();
+    }
     if(direction == 'v'){
         let move = checkValidMove(i,j,l,p);
         if(move==true){
